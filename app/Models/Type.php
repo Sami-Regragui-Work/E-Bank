e3a5a4ebb29eb855e1b90245b78be006fdc6518f
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-03-24 14:14:13 +0100

    feat(models): added Type model

---
+++ app/Models/Type.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'overdraft_limit',
+        'monthly_fee',
+        'interest_rate',
+        'default_daily_transaction_limit',
+        'default_monthly_withdrawal_limit',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'overdraft_limit' => 'decimal:2',
+            'monthly_fee' => 'decimal:2',
+            'interest_rate' => 'decimal:4',
+            'default_daily_transaction_limit' => 'decimal:2',
+            'default_monthly_withdrawal_limit' => 'integer',
+        ];
+    }
+
+    public const COURANT = 'COURANT';
+    public const EPARGNE = 'EPARGNE';
+    public const MINEUR = 'MINEUR';
+
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
+}
