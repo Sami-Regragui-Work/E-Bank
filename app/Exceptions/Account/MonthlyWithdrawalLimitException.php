f29c81b0496df52282f1b2871f2c33de20ebaa49
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-03-26 11:07:55 +0100

    feat(exceptions): added MonthlyWithdrawalLimitException

---
+++ app/Exceptions/Account/MonthlyWithdrawalLimitException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions\Account;
+
+use App\Exceptions\AppException;
+
+class MonthlyWithdrawalLimitException extends AppException
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'The monthly withdrawal limit has been reached.',
+            'MONTHLY_WITHDRAWAL_LIMIT_REACHED',
+            409
+        );
+    }
+}
