61822f496f0e4e7f991d6e4d0711595e853d3ed3
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-03-26 11:31:18 +0100

    feat(interfaces): added AccountRepositoryInterface

---
+++ app/AccountRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+interface AccountRepositoryInterface
+{
+    public function allForUser(User $user): Collection;
+
+    public function findByRib(string $rib): ?Account;
+
+    public function countMonthlyWithdrawals(Account $account): int;
+
+    public function sumDailyTransfers(Account $account): float;
+}
