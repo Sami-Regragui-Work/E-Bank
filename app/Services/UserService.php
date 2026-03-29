7b9e866a45d0326ac7db0369056e9265e8b42891
Sami-Regragui-Work <sami-regragui-work@protonmail.com>
2026-03-29 19:12:45 +0100

    feat(services): added UserService

---
+++ app/Services/UserService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\Auth\InvalidCredentialsException;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    public function __construct()
+    {
+        Auth::shouldUse('api');
+    }
+
+    public function getProfile(): User
+    {
+        return Auth::user();
+    }
+
+    public function updateProfile(
+        string $firstName,
+        string $lastName,
+        ?string $birthdate = null
+    ): User {
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+
+        $user->update([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'birthdate' => $birthdate,
+        ]);
+
+        return $user->fresh();
+    }
+
+    public function changePassword(string $currentPassword, string $newPassword): void
+    {
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+
+        if (!Hash::check($currentPassword, $user->password)) {
+            throw new InvalidCredentialsException();
+        }
+
+        $user->update(['password' => $newPassword]);
+    }
+}
